3568ee1f427fe903ecbe8935bbcd17e62755424d
yasirshahid <yasirshahid5@gmail.com>
2020-09-14 19:30:37 +0500

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import {Typography, Avatar,Grid,Box } from "@material-ui/core";
 import Typed from "react-typed";
 import yasirProfile from "../yasirProfile.png";
+import { CompassCalibrationRounded } from "@material-ui/icons";
 
 // CSS styles
 
@@ -12,18 +13,38 @@ const useStyles = makeStyles(theme => ({
         height: theme.spacing(15),
         margin: theme.spacing(1),
 
+    },
+    title:{
+       color: "tomato",
+       fontFamily: "'Baloo 2', cursive;" 
+    },
+    subTitle: {
+        color: "tan",
+        marginBottom: "3rem",
+        fontFamily: "'Baloo 2', cursive;"
+    },
+    typedContainer:{
+        position:"absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+        width: "100vw",
+        textAlign: "center",
+        zIndex:1
     }
 }));
 const Header = () => {
     const classes = useStyles();
     return(
-        <Box>
-            <Avatar className={classes.avatar} src={yasirProfile} alt="Yasir shahid"/>
-            <Typography  variant="h4">
+        <Box className={classes.typedContainer}>
+            <Grid container justify ="center">
+                <Avatar className={classes.avatar} src={yasirProfile} alt="Yasir shahid"/>   
+            </Grid>
+            <Typography className={classes.title}  variant="h4">
                 <Typed strings={["Yasir Shahid"]} typeSpeed={40}/>
             </Typography>
             <br/>
-            <Typography  variant="h4">
+            <Typography className={classes.subTitle}  variant="h4">
                 <Typed strings={["Design & Animation","Web Development","MERN Stack"]} 
                 typeSpeed={40}
                 backSpeed={60}
