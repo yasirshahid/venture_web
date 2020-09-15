81a09c5dd3f45acf5b279d354268788cb8088f93
yasirshahid <yasirshahid5@gmail.com>
2020-09-15 17:03:36 +0500

    changes done

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,35 @@
 import React from'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import Particles from "react-particles-js";
+import {makeStyles} from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles({
+    particalCanva: {
+        position: "fixed"
+    }
+})
+
 
 const Home=()=>{
+    const classes = useStyles()
     return(
-        <div>
-            <Navbar/>
-            <Header/>
-        </div>
+        <>
+         <Navbar/>
+         <Header/>
+         <Particles
+           canvasClassName= {classes.particalCanva}
+           params={{
+               particles:{
+                  number:{
+                      value: 30
+                  } 
+               }
+           }}
+         />
+         
+        </>
         
 
     );
