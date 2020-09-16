16ef1cc66aeabeb63c549eb7e1b556b6ca4cb238
yasirshahid <yasirshahid5@gmail.com>
2020-09-16 20:25:43 +0500

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const About = () =>{
     return(
-        <div>
-            <h1>Hello about</h1>
-        </div>
+        <>
+          <Navbar/>
+          <h1>Hello about</h1>
+        </>
     )
 };
 
