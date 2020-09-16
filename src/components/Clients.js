47a560ce1f4957146297d528a5401e78323f03ba
yasirshahid <yasirshahid5@gmail.com>
2020-09-16 20:26:38 +0500

    add client list

---
+++ src/components/Clients.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Clients = () => {
+    return(
+        <div>
+            <Navbar/>
+            <h1>
+                Hello Clients
+            </h1>
+        </div>
+
+    )
+};
+
+export default Clients;
