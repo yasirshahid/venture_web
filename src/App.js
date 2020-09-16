ffffbe860afa048896c0bf71fbbb97a6750ce160
yasirshahid <yasirshahid5@gmail.com>
2020-09-16 17:06:50 +0500

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {Route} from "react-router-dom"
 import './App.css';
 import Home from "./components/index";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Services from "./components/Services";
+import Work from"./components/Work";
+import About from"./components/About";
+import Contact from"./components/Contact";
 
 
 function App() {
   return (
     < >
     <CssBaseline/>
-    <Home/>
+    <Route exact path="/" component={Home}/>
+    <Route path="/Services" component={Services}/>
+    <Route path="/Work" component={Work}/>
+    <Route path="/About" component={About}/>
+    <Route path="/Contact" component={Contact}/>
     </>
   );
 }
