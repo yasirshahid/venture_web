cd786eb91374198d28dc370c6cd6c882073e7480
yasirshahid <yasirshahid5@gmail.com>
2020-09-16 20:27:41 +0500

    update

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,43 @@
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles"
+import project1 from "../images/html-css-javascript-lg.jpg"
+import {
+    Box,
+    Grid,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography
+} from "@material-ui/core"
+import Navbar from "./Navbar";
 
 const Work=()=>{
     return(
-        <div>
-            <h1>Hello work</h1>
-        </div>
+      <Box component="div">
+        <Navbar/>
+        <Grid container justify="center" alignItems="center">
+              {/* {Project1} */}
+              <Grid item x5={12} sm={8} md={6}>
+                <Card>
+                  <CardActionArea>
+                      <CardMedia 
+                        component="img" 
+                        alt="Project 1" 
+                        height ="140" 
+                        image={project1}
+                      />
+                  </CardActionArea>
+              </Card>
+              </Grid>
+        </Grid>
+
+      </Box>
+          
+          
+        
     )
 };
 
