86575ccee30b4ff0d3c3cac2247ffae93c291165
yasirshahid <yasirshahid5@gmail.com>
2020-09-16 17:05:46 +0500

    add service component

---
+++ src/components/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Services = () =>{
+    return(
+        <div>
+            <h1 fontColor="#E67E22">Hello Service</h1>
+        </div>
+    )
+};
+
+export default Services;
